9f0397341d1f2ac988ec5faf7ef9df11b9b54cb6
mahabub elahi <mashojibmahabub630@gmail.com>
2022-11-04 12:27:57 +0000

    Initial commit with expense and expense category mcr

---
+++ database/migrations/2022_11_04_122031_create_expenses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->uuid();
+            $table->integer('expense_amount')->unsigned()->nullable(false)->default(12);
+            $table->string('note')->nullable(true)->default(12);
+            $table->string('transaction_account')->nullable(false)->default(12);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
